7a5f6793726bd6ba1b2f0d78a2ea12231397a6a7
= <sepand.haghighi@yahoo.com>
2015-10-20 15:49:03 +0330

    feat : new symbol for not operation added and some minor edit

--- python/generator_2.py
+++ python/generator_2.py
@@ -5,7 +5,8 @@ variable=list(string.ascii_letters)  # Convert Ascii Letter To List
 operation=["'",'+',".","^"]  # Valid Operation
 
 # This Function Create Gate Generation
-def Gate_Gen(gate_number,var_num,not_off=True):
+def Gate_Gen(gate_number,var_num,not_off=True,operator=False):
+    ' Default Sign For Not Operation Is ! and this operation control by operator arguemnt'
     gate_index=0  # Gate Index That Increase In Loop
     gate_list=[] # Empty List Of Gate
     while(gate_index<gate_number):
@@ -18,9 +19,15 @@ def Gate_Gen(gate_number,var_num,not_off=True):
             var_2=variable[randrange(0,var_num)]
         if not_off==False: # Check Not_False Argument
             if coin_1==1:
-                var_1=var_1+"'"
+                if operator==False:
+                    var_1="!"+var_1
+                else:
+                     var_1=var_1+"'"
             if coin_2==1:
-                var_2=var_2+"'"
+                if operator==False:
+                    var_2="!"+var_2
+                else:
+                    var_2=var_2+"'"
             
         op_index_not=randrange(1,4) # Operation Factor
         #coin_3=randrange(0,2)
@@ -29,16 +36,21 @@ def Gate_Gen(gate_number,var_num,not_off=True):
         gate_index=gate_index+1 # increase Gate_index
     return gate_list
 # Generator Final String
-def generator(gate_list,not_off=True):
+def generator(gate_list,not_off=True,operator=False):
+    ' Default Sign For Not Operation Is ! and this operation control by operator arguemnt'
     temp=gate_list # Copy OF The Input Gate List
     temp_2=[] # Empty List
     while(len(temp)>2): # Do This While Until We Have Two Object Process
         i=0 # Counter
         coin_not=randrange(0,2) # Random Number For Add Not At The End Of Each Paranteces
-        if coin_not==1: # Condition Of Not Adding
-            temp[i]=temp[i]+"'"  # Add Not To The First
+        if not_off==False: # Check Condition For Adding Not
+            if coin_not==1: # Probability Of Adding Not
+                if operator==False: # Check Operator Condition (Flse Adding ! and True Adding ' as Not Operator)
+                    temp[i]="!"+temp[i]  
+                else:
+                    temp[i]=temp[i]+"'"  # Add Not To The First
         if len(temp)%2==0: # Even Length
-            while((i+2)<=len(temp)):
+            while((i+2)<=len(temp)): # Check The Condition For Raising End Of The Gate List
                 op_index=operation[randrange(1,4)] # Generate Random Operation (-Not In This Version)
                 temp_2.append("("+temp[i]+op_index+temp[i+1]+")")
                 i=i+2
@@ -69,10 +81,10 @@ if __name__=="__main__":
         file=open(current_dir+"\\Test\\"+file_name,"w")
         
     gate_number=input("Please Enter Number Of 2-Input Gates in File : ")
-    var_number=input("Please Enter Total Number Of Var in File")
-    gate_form=Gate_Gen(int(gate_number),int(var_number))
+    var_number=input("Please Enter Total Number Of Var in File (Larger Than 1 ) : ")
+    gate_form=Gate_Gen(int(gate_number),int(var_number),not_off=False)
     print(gate_form)
-    gen_function=generator(gate_form)
+    gen_function=generator(gate_form , not_off=False)
     file.write(gen_function)
     file.close()
     print("Function : "+gen_function)
